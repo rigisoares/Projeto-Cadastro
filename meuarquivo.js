b5bd2f3b56aefb4926dd0205817e458480a9e184
rigisoares <69166346+rigisoares@users.noreply.github.com>
2020-08-21 09:35:51 -0300

    comando em javascript em outro arquivo
    utilização de comando getElementById em outro arquivo de javascript

---
+++ meuarquivo.js
@@ -0,0 +1,3 @@
+//utilização do comando getElementById para estilizar um botão no arquivo html
+//utilizando arquivo javascript separado do arquivo html
+document.getElementById("bt").style.background="#add8e6";
